802330af2d98257542a91b0c7cbc04325ccf0474
B1GGUN <45312468+B1GGUN@users.noreply.github.com>
2022-02-27 21:52:59 +0800

    Create ECAblock.py

---
+++ ECAblock.py
@@ -0,0 +1,25 @@
+import math
+import torch
+import torch.nn as nn
+
+
+class ECABlock(nn.Module):
+    def __init__(self, channels, gamma = 2, b = 1):
+        super(ECABlock, self).__init__()
+        kernel_size = int(abs((math.log(channels, 2) + b) / gamma))
+        kernel_size = kernel_size if kernel_size % 2 else kernel_size + 1
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.conv = nn.Conv1d(1, 1, kernel_size = kernel_size, padding = (kernel_size - 1) // 2, bias = False)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        v = self.avg_pool(x)
+        v = self.conv(v.squeeze(-1).transpose(-1, -2)).transpose(-1, -2).unsqueeze(-1)
+        v = self.sigmoid(v)
+        return x * v
+
+
+if __name__ == "__main__":
+    features_maps = torch.randn((8, 54, 32, 32))
+    model = ECABlock(54, gamma = 2, b = 1)
+    model(features_maps)
